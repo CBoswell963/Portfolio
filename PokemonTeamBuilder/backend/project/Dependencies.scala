2a524b3078930ba4340a04e0ad13e300a02e3f36
Caleb Boswell <caboswell@rocketmail.com>
2025-05-15 13:41:19 -0400

    set up initial sbt build

--- PokemonTeamBuilder/backend/project/Dependencies.scala
+++ PokemonTeamBuilder/backend/project/Dependencies.scala
@@ -1,5 +1,30 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+
+  val http4sVersion = "0.23.24"
+  val circeVersion  = "0.14.6"
+  val doobieVersion = "1.0.0-RC4"
+
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+
+  val http4s = Seq(
+    "org.http4s" %% "http4s-dsl"          % http4sVersion,
+    "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+    "org.http4s" %% "http4s-circe"        % http4sVersion
+  )
+
+  val circe = Seq(
+    "io.circe" %% "circe-core"    % circeVersion,
+    "io.circe" %% "circe-generic" % circeVersion,
+    "io.circe" %% "circe-parser"  % circeVersion
+  )
+
+  val doobie = Seq(
+    "org.tpolecat" %% "doobie-core"     % doobieVersion,
+    "org.tpolecat" %% "doobie-hikari"   % doobieVersion,
+    "org.tpolecat" %% "doobie-postgres" % doobieVersion
+  )
+
+  val all = http4s ++ circe ++ doobie :+ scalaTest
 }
