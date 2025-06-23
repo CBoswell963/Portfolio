a0317a682e4619c590b9d166e0afcb2c7589ba8d
Caleb Boswell <caboswell@rocketmail.com>
2025-06-23 13:57:31 -0400

    further method implementations in team class and adjustments to type class

--- PokemonTeamBuilder/pokemonteambuilder/models/team.py
+++ PokemonTeamBuilder/pokemonteambuilder/models/team.py
@@ -1,16 +1,22 @@
 # Team class model
 
 from pokemon import Pokemon
+from type import Type
 
 class Team:
 
     # Max number of pokemon a team can have
     MAX_POKEMON = 6
 
+    # number of pokemon types
+    TYPES = 18
+
     # Creates a team object, team name is automatically assigned based on current user's team count if none is provided
     def __init__(self, user):
         # unique id of user (implemented later)
         self.user = user
+        self.attack_type_coverage = [0] * self.TYPES
+        self.defense_type_coverage = [0] * self.TYPES
         self.pokemon_team = []
 
     # Creates a team based on the user's input
@@ -22,15 +28,41 @@ class Team:
         for p in pokemon:
             self.pokemon_team.append(p)
 
+    # returns the pokemon in the team
     def return_team(self):
         return self.name, self.pokemon_team
     
+    # update the pokemon in the team and resets type coverage
     def update_pokemon(self, *pokemon: Pokemon):
         if len(pokemon) > self.MAX_POKEMON:
             raise ValueError("A team can have a maximum of 6 Pokemon.")
         
+        self.attack_type_coverage = [0] * self.TYPES
+        self.defense_type_coverage = [0] * self.TYPES
         self.pokemon_team = pokemon
 
+    # updates the team's name
     def update_name(self, name):
         self.name = name
 
+    # returns a type coverage analysis as an int list based on the pokemon in the team
+    def type_coverage_analysis(self):
+        
+        strong_attack = []
+        weak_attack = []
+        strong_defense = []
+        weak_defense = []
+
+        for pokemon in self.pokemon_team:
+            for type in pokemon.get_types():
+                # put code to search database for matching type and get weakness
+                # and strength lists and assign them to variables here
+                for i in self.attack_type_coverage:
+                    self.attack_type_coverage[i] += strong_attack[i]
+                    self.attack_type_coverage[i] -= weak_attack[i]
+
+                for i in self.defense_type_coverage:
+                    self.defense_type_coverage[i] += strong_defense[i]
+                    self.defense_type_coverage[i] -= weak_defense[i]
+
+        return self.attack_type_coverage, self.defense_type_coverage
--- PokemonTeamBuilder/pokemonteambuilder/models/type.py
+++ PokemonTeamBuilder/pokemonteambuilder/models/type.py
@@ -1,4 +1,8 @@
 # type class model
+# Type list is represented as a string of ints with each index corresponding
+# to a different type
+# List map: [Normal, Fire, Water, Electric, Grass, Ice, Fighting, Poison, Ground,
+# Flying, Psychic, Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy]
 
 class Type:
 
@@ -9,14 +13,14 @@ class Type:
         self.strong_defense = strong_defense
         self.weak_defense = weak_defense
 
-    def get_strong_attack(self) -> list[str]:
+    def get_strong_attack(self) -> list[int]:
         return self.strong_attack
 
-    def get_weaK_attack(self) -> list[str]:
+    def get_weaK_attack(self) -> list[int]:
         return self.weak_attack
     
-    def get_strong_defense(self) -> list[str]:
+    def get_strong_defense(self) -> list[int]:
         return self.strong_defense
     
-    def get_weak_defernse(self) -> list[str]:
+    def get_weak_defernse(self) -> list[int]:
         return self.weak_defense
